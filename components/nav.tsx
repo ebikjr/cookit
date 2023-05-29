255cdf8631509e9ad50b18bd276355f391f35adb
ebikjr <ebikjrinc@gmail.com>
2023-05-29 14:12:45 +0700

    remove apik key

--- components/nav.tsx
+++ components/nav.tsx
@@ -121,63 +121,7 @@ export default function Nav() {
         >
           <TwitterSolid className="w-5 h-5 opacity-75 fill-slate-400 dark:fill-slate-200" />
         </a>
-        <Dialog
-          open={open}
-          onOpenChange={(open) => {
-            setOpen(open);
-            event({
-              action: "toggle_api_modal",
-              category: "user_interaction",
-              label: "User toggles the set api modal",
-              value: open,
-            });
-          }}
-        >
-          <DialogTrigger asChild>
-            <Button
-              variant="outline"
-              className={cn({
-                "animate-ring ease-in-out direction-alternate delay-100 justify-self-end":
-                  !tokenSaved,
-              })}
-            >
-              Add Open AI API Key
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Add Open AI Token</DialogTitle>
-              <DialogDescription className="font-lato">
-                In order to this app to work, you should set{" "}
-                <a
-                  className="text-purple-500 underline"
-                  href="https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  your Open AI API Key
-                </a>
-                . Don&apos;t worry, we will never store it anywhere else than
-                your browser.
-              </DialogDescription>
-            </DialogHeader>
-            <form className="py-4" onSubmit={handleSubmit(onSubmit)}>
-              <Label
-                htmlFor="token"
-                className="text-right dark:text-white font-lato"
-              >
-                Your Open AI API Key
-              </Label>
-              <Input id="token" {...register("token")} className="col-span-3" />
-              <span className="block mt-2 min-h-[20px] text-sm font-light text-rose-600">
-                {errors.token?.message}
-              </span>
-              <Button type="submit" className="block mt-5 ml-auto">
-                Save changes
-              </Button>
-            </form>
-          </DialogContent>
-        </Dialog>
+        
       </div>
     </header>
   );
